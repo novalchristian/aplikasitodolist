cf7e661414208005a4ecf4ed97bb4c01e7716256
novalchristian <novalchristian41@gmail.com>
2020-11-23 16:26:58 +0700

    third commit

--- src/pages/home-detail.page.js
+++ src/pages/home-detail.page.js
@@ -3,44 +3,80 @@ import {Text, View, StyleSheet, Dimensions, TextInput, FlatList} from 'react-nat
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import ToDoItem from './toDoItem';
 import AddToDo from './addToDo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function HomeDetail({route, navigation}) {
-  const [todos, setTodos] = useState([]);
+export default class HomeDetail extends Component {
+  arr=[]
+  id=0
+  state = {
+    text: '',
+    item:[
+      {id:1, data:"loading"}
+    ]
+  };
 
-  const pressHandler = (key) => {
-    setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.key != key)
-    })
-  }
+    storedata = async () =>{
+      this.arr.push({id:this.id, data:this.state.text})
+      this.id++;
+      await AsyncStorage.setItem("mylist", JSON.stringify(this.arr))
+      this.setState({
+        item: JSON.parse(await AsyncStorage.getItem("mylist"))
+      })
+      console.log(this.state)
+    }
 
-  const submitHandler = (text) => {
-    setTodos((prevTodos) => {
-      return [
-        {text: text, key: Math.random().toString()},
-        ...prevTodos
-      ];
-    })
-  }
+    async componentDidMount(){
+      this.setState({
+        item: JSON.parse(await AsyncStorage.getItem("mylist")) || ""
+      })
+      this.arr=JSON.parse(await AsyncStorage.getItem("mylist")) || []
+      this.id = this.arr[this.arr.length-1].id + 1;
+    }
 
-  const {day} = route.params;
-  return (
-    <View style={styles.container}>
-      <View style={styles.topWrapper}>
-        <Text style={styles.title}>{day}</Text>
+  render(){
+    if(this.state.item.length > 0){
+      renderList = this.state.item.map(item=>{
+      return (
+        <TouchableOpacity key={item.id} onPress={()=>{AsyncStorage.removeItem("mylist")}}>
+          <Text style={styles.item} >{item.data}</Text>
+        </TouchableOpacity>
+      )
+      })
+    }else{
+      renderList = <Text style={styles.noItems}>No Items</Text>
+    }
+    return (
+      <View style={styles.container}>
+        <View style={styles.topWrapper}>
+          <Text style={styles.title}>List</Text>
+        </View>
+
+        <View style={styles.inputWrapper}>
+          <TextInput
+            style={styles.textInput}
+            placeholder='Apa yang ingin kamu lakukan?'
+            value={this.state.text}
+            onChangeText={text => this.setState({text})}
+          />
+          <TouchableOpacity style={styles.button} onPress={this.storedata} >
+              <Text style={styles.buttonText}>Input</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          {renderList}
+        </View>
       </View>
-      <AddToDo submitHandler={submitHandler}/>
-      <FlatList 
-        data={todos}
-        renderItem={({item}) => (
-          <ToDoItem item={item} pressHandler={pressHandler} />
-        )}
-      />
-    </View>
-  );
+  
+  )};
 }
 
 const {width, height} = Dimensions.get('screen');
 const styles = StyleSheet.create({
+  noItems:{
+    textAlign: 'center',
+    paddingTop: 50,
+    fontSize: 20,
+  },
   container: {
     width,
   },
@@ -55,5 +91,38 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'dodgerblue',
   },
+  item:{
+    padding:16,
+    marginTop: 16,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderRadius: 10,
+    backgroundColor: '#fff',
+  },
+  inputWrapper:{
+    flexDirection: 'row', 
+    marginHorizontal:20, 
+    marginTop:20, 
+    justifyContent: 'space-between'
+  },
+  textInput:{
+    flex:3,
+    backgroundColor: '#fff',
+    borderRadius: 50,
+    paddingLeft: 10,
+  },
+  button:{
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    flex:1, 
+    backgroundColor: 'green', 
+    borderRadius: 50, 
+    marginLeft:10, 
+    width:80
+  },
+  buttonText:{
+    color: '#fff'
+  }
   
 });
